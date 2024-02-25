2f2c9914802f73acda921238d5badd526bd4d7c6
muhmunt <muhmunt@gmail.com>
2024-02-25 15:48:00 +0700

    added pizza test'

--- main_test.go
+++ main_test.go
@@ -63,3 +63,62 @@ func TestGetAllUrl(t *testing.T) {
 	var response map[string]string
 	err = json.Unmarshal(rr.Body.Bytes(), &response)
 }
+
+func TestPlaceOrder(t *testing.T) {
+	order := entity.Order{
+		Pizza: "Pizza Cheese",
+	}
+
+	orderJSON, err := json.Marshal(order)
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/pizza/orders", bytes.NewBuffer(orderJSON))
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+
+	c, _ := gin.CreateTestContext(rr)
+	c.Request = req
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	var response map[string]string
+	err = json.Unmarshal(rr.Body.Bytes(), &response)
+}
+
+func TestStoreChef(t *testing.T) {
+	chef := entity.Chef{
+		Name: "John",
+	}
+
+	orderJSON, err := json.Marshal(chef)
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/pizza/chef", bytes.NewBuffer(orderJSON))
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+
+	c, _ := gin.CreateTestContext(rr)
+	c.Request = req
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	var response map[string]string
+	err = json.Unmarshal(rr.Body.Bytes(), &response)
+}
+
+func TestGetMenu(t *testing.T) {
+	req, err := http.NewRequest("GET", "/pizza/menus", nil)
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+
+	c, _ := gin.CreateTestContext(rr)
+	c.Request = req
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	var response map[string]string
+	err = json.Unmarshal(rr.Body.Bytes(), &response)
+}
